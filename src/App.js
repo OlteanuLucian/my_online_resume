757cc5fb6ad667e774f704a15f0ac1c2ef153f94
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-05-17 21:55:25 +0300

    Header done v01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
-
+import Header from './components/Header';
 
 
 function App() {
   return (
-
-    <Navbar />
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 20,
+              density: {
+                enable: true,
+                value_area: 1000
+              }
+            },
+            color: {
+              value: "#fff"
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
 
 
 
