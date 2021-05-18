c84f969df657c53d13cff83e8ea2c715de603d0f
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-05-18 15:06:24 +0300

    style experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,15 +5,43 @@ import React from "react"
 const Experience = () => {
     return (
         <div className="experience">
-            <div className="d-flex justify-content-center my5">
+            <div className="d-flex justify-content-center my-5">
                 <h1>experience</h1>
             </div>            
-        
-
-
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className = "timeline-content">
+                        <h3>2020 - current</h3>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+                        veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className = "timeline-content">
+                        <h3>2018-2020</h3>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+                        veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className = "timeline-content">
+                        <h3>2014-2018</h3>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+                        veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Experience
+export default Experience;
 
