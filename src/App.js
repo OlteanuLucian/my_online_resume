83ec2e2070f5b95b9ce188a70ded84c2b8b79426
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-05-20 10:46:23 +0300

    added portfolio section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
-
+import Portfolio from "./components/Portfolio";
 
 
 function App() {
@@ -46,6 +46,8 @@ function App() {
       <Services />
 
       <Experience />
+
+      <Portfolio />
     </>
 
 
