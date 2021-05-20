81ced1ce4f262494abb38ccb0696a1a1494346e8
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-05-20 11:46:43 +0300

    installed react popupbox

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import blog from "../images/mern-blog.png";
+import cityGuide from "../images/city-guide-app.png";
+import netflix from "../images/netflix.png";
+import portfolio from "../images/portfolio.png";
+import taskManager from "../images/task-manager.png"
+//IMPORT FONT AWESOME
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
 
 const Portfolio = () => {
     return (
@@ -6,7 +18,35 @@ const Portfolio = () => {
             <div className = "container">
                 <h1 className ="text-uppercase text-center py-5">portfolio</h1>
                 <div className = "image-box-wrapper row justify-content-center">
-                    <img className = "portfolio-img" src/>
+                    <div className = "portfolio-img-box">
+                        <img className = "portfolio-img" src = {netflix} alt ="Netflix Clone Project"/>
+                        <div className = "overflow"></div>
+                        <FontAwesomeIcon className = "portfolio-icon" icon = {faSearchPlus}/>
+                    </div>
+                        {/* - */}
+                    <div className = "portfolio-img-box">
+                        <img className = "portfolio-img" src = {cityGuide} alt ="City Guide Project"/>
+                        <div className = "overflow"></div>
+                        <FontAwesomeIcon className = "portfolio-icon" icon = {faSearchPlus}/>
+                    </div>
+                        {/* - */}
+                    <div className = "portfolio-img-box">
+                        <img className = "portfolio-img" src = {portfolio} alt ="Portfolio Project"/>
+                        <div className = "overflow"></div>
+                        <FontAwesomeIcon className = "portfolio-icon" icon = {faSearchPlus}/>
+                    </div>
+                        {/* - */}
+                    <div className = "portfolio-img-box">
+                        <img className = "portfolio-img" src = {taskManager} alt ="Task Manager Project"/>
+                        <div className = "overflow"></div>
+                        <FontAwesomeIcon className = "portfolio-icon" icon = {faSearchPlus}/>
+                    </div>
+                        {/* - */}
+                    <div className = "portfolio-img-box">
+                        <img className = "portfolio-img" src = {blog} alt ="Blog Project"/>
+                        <div className = "overflow"></div>
+                        <FontAwesomeIcon className = "portfolio-icon" icon = {faSearchPlus}/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -14,4 +54,4 @@ const Portfolio = () => {
     )
 }
 
-export default Portfolio
+export default Portfolio;
