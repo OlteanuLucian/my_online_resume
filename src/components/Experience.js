360641366b8479edc6fa2ad24cc7742ee8d8a067
lucian.olteanu <vlucian.olteanu@gmail.com>
2021-05-25 18:20:59 +0300

    checking education

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,7 @@ const Experience = () => {
     return (
         <div className="experience">
             <div className="d-flex justify-content-center my-5">
-                <h1>experience</h1>
+                <h1>eductation</h1>
             </div>            
             <div className="container experience-wrapper">
                 <div className="timeline-block timeline-block-right">
